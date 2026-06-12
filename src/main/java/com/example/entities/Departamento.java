82f4badb9697dfe744915fab64e3c68757ba9d88
vrma <vrmachado@gmail.com>
2026-06-12 13:20:13 +0200

    entidades relacionadas

--- src/main/java/com/example/entities/Departamento.java
+++ src/main/java/com/example/entities/Departamento.java
@@ -1,15 +1,19 @@
 package com.example.entities;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 @Entity
 @Table(name="departamentos")
 @NoArgsConstructor
@@ -22,4 +26,18 @@ public class Departamento {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
     private String nombre;
+
+    /**
+     * Las relaciones entre las entidades en JPA (Java Persistence API) son bidireccionales,
+     * a diferencia de las relaciones en SQL que son unidireccionales, es decir, que una 
+     * entidad hija sabe quien es su padre, porque en dicha entidad es donde se crea la relacion
+     * de clave externa (FK), pero la entidad padre no sabe que tiene hijo(s)
+     */
+
+    /* El atributo mappedBy apunta a una propiedad en el lado de muchos de la relacion, pues
+    aunque las relaciones son bidireccionales hay que especificar donde se va a crear 
+    la relacion de clave externa, que al igual que en SQL es en el lado de muchos */
+    
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE, mappedBy = "departamento")
+    private List<Empleado> empleados;
 }
--- src/main/java/com/example/entities/Empleado.java
+++ src/main/java/com/example/entities/Empleado.java
@@ -2,17 +2,22 @@ package com.example.entities;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Set;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
 import com.example.model.Genero;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -42,5 +47,14 @@ public class Empleado {
     private LocalDate fechaAlta;
 
     private BigDecimal salario;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Departamento departamento;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE, mappedBy = "empleado")
+    private Set<Telefono> telefonos;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE, mappedBy = "empleado")
+    private Set<Correo> emails;
     
 }
