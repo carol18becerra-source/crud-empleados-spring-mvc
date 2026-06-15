b25d20ec2a8a8f9c4f5a809ac5aac533b32e92a5
vrma <vrmachado@gmail.com>
2026-06-15 10:19:22 +0200

    capa dao implementada

--- src/main/java/com/example/dao/EmpleadoDao.java
+++ src/main/java/com/example/dao/EmpleadoDao.java
@@ -24,6 +24,7 @@ public interface EmpleadoDao extends JpaRepository<Empleado, Integer> {
 	 * Buscar JPA Query Methods */
  
 	List<Empleado> findByNombre(String nombre);
+	
 	boolean existsByGenero(Genero genero);
 	
 }
